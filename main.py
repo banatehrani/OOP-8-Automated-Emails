f47c2069cf3d3461b3b1ea8ec0e41454a35a1273
banatehrani <banatehrani@gmail.com>
2021-12-06 16:14:18 -0500

    Basic idea and the news API link created.

---
+++ main.py
@@ -0,0 +1,29 @@
+# API key: 172a5c3a01724bbda2cb9ac2cf824816
+
+import requests
+from pprint import pprint
+
+
+class NewsFeed:
+
+    def __init__(self, data):
+        self.data = data
+
+    def get(self):
+        pass
+
+
+# Here in the url we use everything end point then parameters are specified.
+# You can change the value of q in the url to look for another topic.
+# qInTitle looks for the word only in the news title.
+# For the full list of parameters visit: https://newsapi.org/docs/endpoints/everything
+url = "https://newsapi.org/v2/everything?" \
+      "qInTitle=nasa&" \
+      "from=2021-12-05&" \
+      "to=2021-12-06&" \
+      "language=en&" \
+      "apiKey=172a5c3a01724bbda2cb9ac2cf824816"
+
+response = requests.get(url)
+content = response.text
+pprint(content)
