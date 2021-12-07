364939f580cd42cea4d4887ead2eb543651f83d5
banatehrani <banatehrani@gmail.com>
2021-12-06 20:39:26 -0500

    Program will be executed at a certain time.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import yagmail
-import pandas
+import pandas as pd
 import datetime
+import time
 from news import NewsFeed
 
-df = pandas.read_excel('people.xlsx')
+while True:
+    if datetime.datetime.now().hour == 20 and datetime.datetime.now().minute == 38:
 
-for index, row in df.iterrows():
-    news_feed = NewsFeed(interest=row['interest'],
-                         from_date=((datetime.datetime.now() -
-                                     datetime.timedelta(days=1)).strftime('%Y-%m-%d')),
-                         to_date=datetime.datetime.now().strftime('%Y-%m-%d'))
+        df = pd.read_excel('people.xlsx')
 
-    yagmail.SMTP(user="mehdi.python.developer@gmail.com", password="mehdi_python_developer"). \
-        send(to=row['email'],
-             subject=f"{row['interest']} news for today!",
-             contents=f"Hi {row['name']}\n See what's new about {row['interest']} "
-                      f"today. {news_feed.get()}\nPython Developer")
+        for index, row in df.iterrows():
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            yesterday = ((datetime.datetime.now() -
+                          datetime.timedelta(days=1)).strftime('%Y-%m-%d'))
+            news_feed = NewsFeed(interest=row['interest'],
+                                 from_date=yesterday,
+                                 to_date=today)
+
+            yagmail.SMTP(user="mehdi.python.developer@gmail.com", password="mehdi_python_developer"). \
+                send(to=row['email'],
+                     subject=f"{row['interest']} news for today!",
+                     contents=f"Hi {row['name']}\n See what's new about {row['interest']} "
+                              f"today. {news_feed.get()}\nPython Developer")
+
+    time.sleep(60)
