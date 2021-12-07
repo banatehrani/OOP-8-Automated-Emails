e498c859f55f126be56e1bca4985f5fa5ed2cf92
banatehrani <banatehrani@gmail.com>
2021-12-06 20:08:17 -0500

    Automatic emails sent to a list

--- main.py
+++ main.py
@@ -1,62 +1,16 @@
-import requests
+import yagmail
+import pandas
+from news import NewsFeed
 
+df = pandas.read_excel('people.xlsx')
 
-class NewsFeed:
-    """Representing multiple news titles and links as a single string"""
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date='2021-12-05',
+                         to_date='2021-12-06')
 
-    base_url = "https://newsapi.org/v2/everything?"
-    api_key = "172a5c3a01724bbda2cb9ac2cf824816"
-
-    def __init__(self, interest, from_date, to_date, language):
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
-
-    def get(self):
-        url = f"{self.base_url}" \
-              f"qInTitle={self.interest}&" \
-              f"from={self.from_date}&" \
-              f"to={self.to_date}&" \
-              f"language={self.language}&" \
-              f"apiKey={self.api_key}"
-
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
-
-        email_body = ''
-        for article in articles:
-            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
-
-        return email_body
-
-
-news_feed = NewsFeed(interest='nasa', from_date='2021-12-05', to_date='2021-12-06', language='en')
-print(news_feed.get())
-
-# Here in the url we use everything end point then parameters are specified.
-# You can change the value of q in the url to look for another topic.
-# qInTitle looks for the word only in the news title.
-# For the full list of parameters visit: https://newsapi.org/docs/endpoints/everything
-# url = "https://newsapi.org/v2/everything?" \
-#       "qInTitle=nasa&" \
-#       "from=2021-12-05&" \
-#       "to=2021-12-06&" \
-#       "language=en&" \
-#       "apiKey=172a5c3a01724bbda2cb9ac2cf824816"
-#
-# response = requests.get(url)
-# content = response.json()  # The response is in json format and will be converted to dictionary
-# x = content['articles'][2]['title']  # Title of the 3rd article
-# y = content['articles'][2]['url']  # url of the 3rd article
-# pprint(x)
-# pprint(y)
-#
-# articles = content['articles']
-#
-# email_body = ''
-# for article in articles:
-#     email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
-#
-# print(email_body)
+    yagmail.SMTP(user="mehdi.python.developer@gmail.com", password="mehdi_python_developer"). \
+        send(to=row['email'],
+             subject=f"{row['interest']} news for today!",
+             contents=f"Hi {row['name']}\n See what's new about {row['interest']} "
+                      f"today. {news_feed.get()}\nPython Developer")
---
+++ news.py
@@ -0,0 +1,61 @@
+import requests
+
+
+class NewsFeed:
+    """Representing multiple news titles and links as a single string"""
+
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "172a5c3a01724bbda2cb9ac2cf824816"
+
+    def __init__(self, interest, from_date, to_date, language='en'):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
+
+    def get(self):
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              f"apiKey={self.api_key}"
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+
+# news_feed = NewsFeed(interest='nasa', from_date='2021-12-05', to_date='2021-12-06', language='en')
+# print(news_feed.get())
+
+# Here in the url we use everything end point then parameters are specified.
+# You can change the value of q in the url to look for another topic.
+# qInTitle looks for the word only in the news title.
+# For the full list of parameters visit: https://newsapi.org/docs/endpoints/everything
+# url = "https://newsapi.org/v2/everything?" \
+#       "qInTitle=nasa&" \
+#       "from=2021-12-05&" \
+#       "to=2021-12-06&" \
+#       "language=en&" \
+#       "apiKey=172a5c3a01724bbda2cb9ac2cf824816"
+#
+# response = requests.get(url)
+# content = response.json()  # The response is in json format and will be converted to dictionary
+# x = content['articles'][2]['title']  # Title of the 3rd article
+# y = content['articles'][2]['url']  # url of the 3rd article
+# pprint(x)
+# pprint(y)
+#
+# articles = content['articles']
+#
+# email_body = ''
+# for article in articles:
+#     email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+#
+# print(email_body)
